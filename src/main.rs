b1a1e3c2e523c287d82a547653ab4b72a41257f9
lake <102535481@qq.com>
2024-06-11 01:07:16 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ fn main() {
 mod test {
     use std::error::Error;
     use nom::branch::alt;
-    use nom::bytes::complete::{tag, take, take_until, take_while};
-    use nom::character::complete::{alpha0, alphanumeric0, alphanumeric1, char, digit0, digit1, i32, space1};
-    use nom::combinator::map_res;
-    use nom::IResult;
-    use nom::multi::length_count;
-    use nom::sequence::{delimited, preceded, separated_pair, terminated};
+    use nom::bytes::complete::{tag, take, take_until, take_while, take_while_m_n};
+    use nom::character::complete::{alpha0, alphanumeric0, alphanumeric1, char, digit0, digit1, i32, multispace0, one_of, space1};
+    use nom::combinator::{map_res, recognize};
+    use nom::{IResult, Parser};
+    use nom::error::ParseError;
+    use nom::multi::{length_count, many0, many1};
+    use nom::sequence::{delimited, preceded, separated_pair, terminated, Tuple};
 
     //识别字符串
     #[test]
@@ -136,4 +137,61 @@ mod test {
         assert_eq!(output, "hello world");
         Ok(())
     }
+
+    fn hex_primary(input: &str) -> IResult<&str, &str> {
+        map_res(
+            take_while_m_n(1, 1, |c| true),
+            |s: &str| Ok(s) as Result<&str, std::num::ParseIntError>,
+        )(input)
+    }
+
+    #[test]
+    fn test_parse() -> Result<(), Box<dyn Error>> {
+        let (remaing, (a, b, c)) = (hex_primary, hex_primary, hex_primary).parse("abc")?;
+        assert_eq!(remaing, "");
+        assert_eq!(a, "a");
+        assert_eq!(b, "b");
+        assert_eq!(c, "c");
+        Ok(())
+    }
+
+    #[test]
+    fn test_recongnize() -> Result<(), Box<dyn Error>> {
+        let result: IResult<&str, &str> = preceded(
+            alt((tag("0x"), tag("0X"))),
+            recognize(
+                // 使用 `many1` 匹配一个或多个十六进制数字，
+                // 使用 `terminated` 组合器确保每个数字后面可以跟随零个或多个下划线。
+                many1(
+                    terminated(one_of("0123456789abcdefABCDEF"), many0(char('_')))
+                )
+            ),
+        )("0x9c9c9c");
+        let (remaing,output) = result?;
+        Ok(())
+    }
+
+    // #[test]
+    // fn test_ws() -> Result<(), Box<dyn Error>> {
+    //     let result: IResult<&str, &str> = ws(" hi ");
+    //     let (remaing,output) = result?;
+    // assert_eq!(remaing, "hi");
+    // Ok(())
+    // }
+
+    fn ws<'a, F, O, E: ParseError<&'a str>>(inner: F) -> impl Parser<&'a str, O, E>
+        where
+            F: Parser<&'a str, O, E>,
+    {
+        // 使用 nom 提供的 delimited 函数，它接受三个参数：
+        // 1. 在 `inner` 前面要消耗的空白解析器，这里是 multispace0。
+        // 2. 要应用的实际解析器 `inner`。
+        // 3. 在 `inner` 后面要消耗的空白解析器，这里同样是 multispace0。
+        delimited(
+            multispace0,
+            inner,
+            multispace0,
+        )
+    }
 }
+pub struct Name(String);
